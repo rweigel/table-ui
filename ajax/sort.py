e42f7bea866716eceb20fbf4fd3442d6d93c17fd
Bob Weigel <rweigel@gmu.edu>
2023-06-22 14:05:27 -0400

    misc adds

--- ajax/sort.py
+++ ajax/sort.py
@@ -1,14 +1,11 @@
-# Step 2: Define the custom sorting function
-def sorting_key(row, idx):
-  return row[idx]
-
 matrix = [["A","B"],["A","A"],["B","B"]]
-# Step 3: Sort the matrix using the custom sorting function
 
-idx = 0
-key=lambda row: row
-sorted_matrix = sorted(matrix, key=key, reverse=False)
+types = []
 
-# Step 4: Print the sorted matrix
-for row in sorted_matrix:
-  print(row)
+def sort_key(row):
+  return row[0], -ord(row[1])  # Sort second column in descending order
+
+matrix.sort(key=sort_key)
+
+for row in matrix:
+    print(row)
